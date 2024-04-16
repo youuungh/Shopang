e7f17ebf251fdd3f90487c9b3e138befd87e2c26
ninezero <kusepz@gmail.com>
2024-04-17 01:41:17 +0900

    update commit

---
+++ app/src/main/java/com/ninezero/shopang/di/DialogModule.kt
@@ -0,0 +1,39 @@
+package com.ninezero.shopang.di
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import android.view.Window
+import androidx.annotation.RequiresApi
+import com.ninezero.shopang.R
+import com.ninezero.shopang.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.scopes.ActivityScoped
+import eightbitlab.com.blurview.BlurView
+import eightbitlab.com.blurview.RenderEffectBlur
+import javax.inject.Named
+
+@Module
+@InstallIn(ActivityComponent::class)
+class DialogModule {
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    @ActivityScoped
+    @Provides
+    @Named(Constants.LOADING)
+    fun provideLoadingDialog(@ActivityContext context: Context): Dialog {
+        val dialog = Dialog(context)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.loading_dialog_layout)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.create()
+        return dialog
+    }
+}
---
+++ app/src/main/java/com/ninezero/shopang/di/FirebaseModule.kt
@@ -0,0 +1,46 @@
+package com.ninezero.shopang.di
+
+import android.content.Context
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.ninezero.shopang.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class FirebaseModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAuth(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
+    }
+
+    @Singleton
+    @Provides
+    fun provideFirebaseFirestore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+
+    @Singleton
+    @Provides
+    fun provideFirebaseStorage(): FirebaseStorage {
+        return FirebaseStorage.getInstance()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGoogleSignInOptions(@ApplicationContext context: Context): GoogleSignInOptions {
+        return GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(context.getString(R.string.clientId))
+            .requestEmail()
+            .build()
+    }
+}
